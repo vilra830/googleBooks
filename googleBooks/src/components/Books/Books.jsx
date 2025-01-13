af36465277592e51176c518e0966d46a1e448b0f
vilra830 <villafuerte.rhea@gmail.com>
2025-01-14 07:03:20 +1100

    adds Books.jsx into components

---
+++ googleBooks/src/components/Books/Books.jsx
@@ -0,0 +1,14 @@
+const Books = (bookData) => {
+  return (
+    <>
+      <article className={classes.books}>
+        <h3>{bookData.title}</h3>
+        <h4>{bookData.author}</h4>
+        {image && <img src={bookData.image} alt={bookData.title} />}
+        <p>{bookData.description}</p>
+      </article>
+    </>
+  );
+};
+
+export default Books;
